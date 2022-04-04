e3d039d1481533ea5282b46700aa3dc687eddc4d
ndricca <andrea.ricca@hotmail.it>
2022-04-04 18:51:06 +0200

    added relative visualization to overview figures

--- social_analysis/main_dashboard.py
+++ social_analysis/main_dashboard.py
@@ -1,6 +1,6 @@
-from dash import Dash, html, Output, Input
+from dash import Dash, html, Output, Input, dcc
 
-from overview_dashboard import contract_callback
+from overview_dashboard import contract_callback, total_time_callback, interactions_callback, hour_of_day_callback
 from social_analysis.evening_dashboard import get_evening_dashboard
 from social_analysis.overview_dashboard import get_overview_dashboards, location_callback, age_callback
 from social_analysis.platform_dashboard import communities_callback, platforms_callback, get_platform_dashboards, \
@@ -8,10 +8,15 @@ from social_analysis.platform_dashboard import communities_callback, platforms_c
 
 app = Dash(__name__)
 
-
 app.layout = html.Div(children=[
     html.H1(children='Presentazione Social Survey'),
-
+    html.H4(children="Visualizza i grafici in:"),
+    dcc.RadioItems(
+        ['Valore Assoluto', 'Valore Relativo'],
+        'Valore Assoluto',
+        id='measure_type',
+        inline=True,
+    ),
     *get_overview_dashboards(),
 
     html.Hr(),
@@ -35,7 +40,22 @@ app.callback(
     Input('measure_type', 'value'),
 )(contract_callback)
 
-callback_input=[
+app.callback(
+    Output('total_time', 'figure'),
+    Input('measure_type', 'value'),
+)(total_time_callback)
+
+app.callback(
+    Output('hour_of_day', 'figure'),
+    Input('measure_type', 'value'),
+)(hour_of_day_callback)
+
+app.callback(
+    Output('interactions', 'figure'),
+    Input('measure_type', 'value'),
+)(interactions_callback)
+
+callback_input = [
     Input('follower_type', 'value'),
     Input('hour_of_day_select', 'value'),
 ]
@@ -70,7 +90,7 @@ app.callback(
     *callback_input,
 )(age_by_platform_callback)
 
-server=app.server
+server = app.server
 
 if __name__ == '__main__':
     app.run_server(debug=True)
--- social_analysis/overview_dashboard.py
+++ social_analysis/overview_dashboard.py
@@ -29,14 +29,49 @@ def contract_callback(measure_type='Valore Assoluto'):
     return contract_fig
 
 
+def total_time_callback(measure_type='Valore Assoluto'):
+    assert measure_type in ('Valore Assoluto', 'Valore Relativo'), f"Invalid measure_type value {measure_type}"
+    histnorm = "probability" if measure_type == 'Valore Relativo' else None
+    total_time_fig = px.histogram(clean_df, x="total_time",
+                                  category_orders={"total_time": [
+                                      "meno di 1 ora",
+                                      "da 1 a 2 ore",
+                                      "da 2 a 3 ore",
+                                      "più di 3 ore"]
+                                  },
+                                  color_discrete_sequence=['green'], histnorm=histnorm
+                                  )
+    return total_time_fig
+
+
+def hour_of_day_callback(measure_type='Valore Assoluto'):
+    assert measure_type in ('Valore Assoluto', 'Valore Relativo'), f"Invalid measure_type value {measure_type}"
+    histnorm = "probability" if measure_type == 'Valore Relativo' else None
+    hour_of_day_fig = px.histogram(exploded_hour_of_day_df, x="hour_of_day",
+                 histfunc="sum",
+                 category_orders={"hour_of_day": [
+                     "mattina",
+                     "pausa pranzo",
+                     "pomeriggio",
+                     "sera"]
+                 },
+                 color_discrete_sequence=['indianred'], histnorm=histnorm
+                 )
+    return hour_of_day_fig
+
+
+def interactions_callback(measure_type='Valore Assoluto'):
+    assert measure_type in ('Valore Assoluto', 'Valore Relativo'), f"Invalid measure_type value {measure_type}"
+    histnorm = "probability" if measure_type == 'Valore Relativo' else None
+    interactions_fig = px.histogram(interactions_df, x="interactions",
+                                    histfunc="sum",
+                                    color_discrete_sequence=['turquoise'], histnorm=histnorm
+                                    )
+    return interactions_fig
+
+
 def get_demo_dash():
     return [html.H3(children='Demografia'),
-            dcc.RadioItems(
-                ['Valore Assoluto', 'Valore Relativo'],
-                'Valore Assoluto',
-                id='measure_type',
-                inline=True,
-            ),
             html.Div([
                 dcc.Graph(
                     id='ages',
@@ -54,46 +89,19 @@ def get_demo_dash():
 
 
 def get_habits_dash():
-    total_time_fig = px.histogram(clean_df, x="total_time",
-                                  category_orders={"total_time": [
-                                      "meno di 1 ora",
-                                      "da 1 a 2 ore",
-                                      "da 2 a 3 ore",
-                                      "più di 3 ore"]
-                                  }, color_discrete_sequence=['green']
-                                  )
-
-    hour_of_day_fig = px.histogram(exploded_hour_of_day_df, x="hour_of_day",
-                                   histfunc="sum",
-                                   category_orders={"hour_of_day": [
-                                       "mattina",
-                                       "pausa pranzo",
-                                       "pomeriggio",
-                                       "sera"]
-                                   }, color_discrete_sequence=['indianred']
-                                   )
-
-    interactions_fig = px.histogram(interactions_df, x="interactions",
-                                    histfunc="sum",
-                                    color_discrete_sequence=['turquoise']
-                                    )
-
     return [
         html.H3(children='Utilizzo'),
 
         html.Div(
             [
                 dcc.Graph(
-                    id='total_time',
-                    figure=total_time_fig
+                    id='total_time'
                 ),
                 dcc.Graph(
-                    id='hour_of_day',
-                    figure=hour_of_day_fig
+                    id='hour_of_day'
                 ),
                 dcc.Graph(
-                    id='interactions',
-                    figure=interactions_fig
+                    id='interactions'
                 ),
             ], style={'display': 'flex', 'flex-direction': 'row'})
 
